a564188570a55de60af5e519a6f1e096098c1090
Scott Hadzik <scotthadzik@weber.edu>
2021-04-05 08:40:44 -0600

    Initial commit

---
+++ calculator.py
@@ -0,0 +1,8 @@
+# Get information from the user about conversion
+# TODO User input validation
+userInput = input('What is the number: ')
+print('Your number is:', userInput) # TODO remove for production
+userNumber = int(userInput) 
+
+# Perform the conversion
+# Print out the answer to user
