73124b2ff27586d15e81e87d1949f4e03d781f44
Scott Hadzik <scotthadzik@weber.edu>
2021-04-12 07:58:26 -0600

    Added feet to inches function

--- calculator.py
+++ calculator.py
@@ -7,11 +7,14 @@
 def MMToin(userNumber):
     return userNumber / 25.4
 
+def inToMM(userNumber):
+    return userNumber * 25.4
+
 def inToFt(userNumber):
     return userNumber / 12
 
-def inToMM(userNumber):
-    return userNumber * 25.4
+def FtToIn(userNumber):
+    return userNumber * 12
         
 def printResults(userConversionInput, userNumber):
     if userConversionInput == '1':
@@ -29,13 +32,20 @@ def printResults(userConversionInput, userNumber):
         conversionUnit = 'in'
         convertedUnit = 'ft'
         calcValue = inToFt(userNumber)
+    if userConversionInput == '4':
+        #set first value to ft and second value to in
+        conversionUnit = 'ft'
+        convertedUnit = 'in'
+        calcValue = FtToIn(userNumber)
     print('Conversion --> ', userNumber, conversionUnit, '=', calcValue, convertedUnit)
 
-
-
-
 while True:
-    userConversionInput = input('What type of conversion? \n\t 1-inches to mm \n\t 2-mm to inches: \n\t 3-inches to feet\n\t Q-to Quit')
+    userConversionInput = input('What type of conversion?'
+    '\n\t 1-inches to mm'
+    '\n\t 2-mm to inches:'
+    '\n\t 3-inches to feet' 
+    '\n\t 4-feet to inches' 
+    '\n\t Q-to Quit\n')
     if userConversionInput == 'Q':
         break
     userInput = input('What is the number: ')
